db1c1fd3568e6a7ac1f61fb7d7eeefba6f1db1fd
a-rkhan <arahmankhan.usa@gmail.com>
2018-03-29 22:51:57 -0400

    maven

---
+++ maven/src/main/java/MyfirstMaven/maven/App.java
@@ -0,0 +1,14 @@
+package MyfirstMaven.maven;
+
+import java.sql.*;
+public class App 
+{
+	static final String JDBC_Driver = "com.mysql.jdbc.Driver";
+	static final String User = "root";
+	static final String Password = "aRkhan9642";
+	
+    public static void main( String[] args )
+    {
+        
+    }
+}
